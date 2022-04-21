a8e90381d8c383afb4be9bd42e771e9f9d56f3e8
CarlosGonzalez20 <ll3224645@gmail.com>
2022-04-21 13:39:07 -0600

    agregando catalogo Sede, sub menus registrar,mostrar,modificar,buscar,eliminar

--- segundoParcial/PrototipoP12P2022-Carlos González.cpp
+++ segundoParcial/PrototipoP12P2022-Carlos González.cpp
@@ -9,7 +9,7 @@ using namespace std;
 class colegio
 {
 	private:
-		string id,name,phone,college,address;
+		string id,name,phone,address, numCur, numSede, numFacultad;
 	public:
 		void menuPrincipal();
 		void catalogos();
@@ -17,11 +17,21 @@ class colegio
 		void facultades();
 		void cursos();
 		void reportes();
-		void insert();
-		void display();
-		void modify();
-		void search();
-		void delet();
+		void insertSede();
+		void displaySede();
+		void modifySede();
+		void searchSede();
+		void deletSede();
+        void insertFacultad();
+		void displayFacultad();
+		void modifyFacultad();
+		void searchFacultad();
+		void deletFacultad();
+        void insertCursos();
+		void displayCursos();
+		void modifyCursos();
+		void searchCursos();
+		void deletCursos();
 };
 
 void colegio::menuPrincipal(){
@@ -30,7 +40,6 @@ void colegio::menuPrincipal(){
 	do
     {
 	system("cls");
-
 	cout<<"\t\t\t--------------------------------"<<endl;
 	cout<<"\t\t\t| BIENVENIDOS AL MENU PRINCIPAL |"<<endl;
 	cout<<"\t\t\t--------------------------------"<<endl;
@@ -58,16 +67,302 @@ void colegio::menuPrincipal(){
 }
 
 void colegio::catalogos(){
-    cout <<"hola";
+    int choice;
+	char x;
+	do
+    {
+	system("cls");
+	cout<<"\t\t\t--------------------------------"<<endl;
+	cout<<"\t\t\t| BIENVENIDOS AL MENU CATALOGOS |"<<endl;
+	cout<<"\t\t\t--------------------------------"<<endl;
+	cout<<"\t\t\t 1. Sedes"<<endl;
+	cout<<"\t\t\t 2. Facultades"<<endl;
+	cout<<"\t\t\t 3. Cursos"<<endl;
+	cout<<"\t\t\t 4. Regresar al MENU PRINCIPAL"<<endl;
+	cout<<"\n\t\t\tIngresa tu Opcion: "; cin>>choice;
+    switch(choice)
+    {
+    case 1:
+    	sedes();
+		break;
+	case 2:
+		facultades();
+		break;
+	case 3:
+		cursos();
+		break;
+    case 4:
+        return menuPrincipal();
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+	}
+	getch();
+    }
+    while(choice!= 4);
 }
 
 void colegio::sedes(){
-    cout <<"hola";
+    int choice;
+	char x;
+	do
+    {
+	system("cls");
+	cout<<"\t\t\t--------------------------------"<<endl;
+	cout<<"\t\t\t|   BIENVENIDOS AL MENU SEDE   |"<<endl;
+	cout<<"\t\t\t--------------------------------"<<endl;
+	cout<<"\t\t\t 1. Registrar"<<endl;
+	cout<<"\t\t\t 2. Mostrar"<<endl;
+	cout<<"\t\t\t 3. Modificar"<<endl;
+	cout<<"\t\t\t 4. Buscar"<<endl;
+	cout<<"\t\t\t 5. Eliminar"<<endl;
+	cout<<"\t\t\t 6. Regresar al MENU CATALOGOS"<<endl;
+	cout<<"\n\t\t\tIngresa tu Opcion: "; cin>>choice;
+    switch(choice)
+    {
+    case 1:
+    	insertSede();
+		break;
+	case 2:
+		displaySede();
+		break;
+	case 3:
+		modifySede();
+		break;
+    case 4:
+        searchSede();
+        break;
+    case 5:
+        deletSede();
+        break;
+    case 6:
+        return catalogos();
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+	}
+	getch();
+    }
+    while(choice!= 6);
+}
+
+void colegio::insertSede(){
+    system("cls");
+	fstream file;
+	cout<<"\n------------------------------------------------------------------------------------------------------------------------";
+	cout<<"\n-------------------------------------------------Agregar detalles Sede -------------------------------------------------"<<endl;
+	cout<<"\t\t\tIngresa Numero Sede        : ";
+	cin>>numSede;
+	cout<<"\t\t\tIngresa Nombre Sede        : ";
+	cin>>name;
+	cout<<"\t\t\tIngresa Telefono Sede      : ";
+	cin>>phone;
+	cout<<"\t\t\tIngresa Direccion Sede     : ";
+	cin>>address;
+	file.open("sede.txt", ios::app | ios::out);
+	file<<std::left<<std::setw(15)<< numSede <<std::left<<std::setw(15)<< name <<std::left<<std::setw(15)<< phone <<std::left<<std::setw(15)<< address << "\n";
+	file.close();
+}
+
+void colegio::displaySede(){
+    system("cls");
+	fstream file;
+	int total=0;
+	cout<<"\n-------------------------Tabla de Detalles de Sedes -------------------------"<<endl;
+	file.open("sede.txt",ios::in);
+	if(!file)
+	{
+		cout<<"\n\t\t\tNo hay información...";
+		file.close();
+	}
+	else
+	{
+		file >> numSede >> name >> phone >> address;
+		while(!file.eof())
+		{
+			total++;
+			cout<<"\n\n\t\t\t Numero Sede: "<<numSede<<endl;
+			cout<<"\t\t\t Nombre Sede: "<<name<<endl;
+			cout<<"\t\t\t Telefono Sede: "<<phone<<endl;
+			cout<<"\t\t\t Direccion Sede: "<<address<<endl;
+			file >> numSede >> name >> phone >> address;
+		}
+		if(total==0)
+		{
+			cout<<"\n\t\t\tNo hay informacion...";
+		}
+	}
+	file.close();
+}
+
+void colegio::modifySede(){
+    system("cls");
+	fstream file,file1;
+	string busquedaSede;
+	int found=0;
+	cout<<"\n-------------------------Modificacion Detalles Sede-------------------------"<<endl;
+	file.open("sede.txt",ios::in);
+	if(!file)
+	{
+		cout<<"\n\t\t\tNo hay informacion..,";
+		file.close();
+	}
+	else
+	{
+		cout<<"\n Ingrese NUMERO de la Sede que quiere modificar: ";
+		cin>>busquedaSede;
+		file1.open("modSede.txt",ios::app | ios::out);
+		file >> numSede >> name >> phone >> address;
+		while(!file.eof())
+		{
+			if(busquedaSede!=numSede)
+			{
+			 file1<<std::left<<std::setw(15)<< numSede <<std::left<<std::setw(15)<< name <<std::left<<std::setw(15)<< phone <<std::left<<std::setw(15)<< address << "\n";
+			}
+			else
+			{
+				cout<<"\t\t\tIngrese Numero Sede: ";
+				cin>>numSede;
+				cout<<"\t\t\tIngrese Nombre Sede: ";
+				cin>>name;
+				cout<<"\t\t\tIngrese Telefono Sede: ";
+				cin>>phone;
+				cout<<"\t\t\tIngrese Direccion Sede: ";
+				cin>>address;
+				file1<<std::left<<std::setw(15)<< numSede <<std::left<<std::setw(15)<< name <<std::left<<std::setw(15)<< phone <<std::left<<std::setw(15)<< address << "\n";
+				found++;
+			}
+			file >> numSede >> name >> phone >> address;
+
+		}
+		file1.close();
+		file.close();
+		remove("sede.txt");
+		rename("modSede.txt","sede.txt");
+	}
+}
+
+void colegio::searchSede(){
+    system("cls");
+	fstream file;
+	int found=0;
+	file.open("sede.txt",ios::in);
+	if(!file)
+	{
+		cout<<"\n-------------------------Datos de la Sede buscada------------------------"<<endl;
+		cout<<"\n\t\t\tNo hay informacion...";
+	}
+	else
+	{
+		string busquedaSede;
+		cout<<"\n-------------------------Datos de la Sede buscada------------------------"<<endl;
+		cout<<"\nIngrese NUMERO de la Sede que quiere buscar: ";
+		cin>>busquedaSede;
+		file >> numSede >> name >> phone >> address;
+		while(!file.eof())
+		{
+			if(busquedaSede==numSede)
+			{
+				cout<<"\n\n\t\t\t Numero de Sede: "<<numSede<<endl;
+				cout<<"\t\t\t Nombre Sede: "<<name<<endl;
+				cout<<"\t\t\t Telefono Sede: "<<phone<<endl;
+				cout<<"\t\t\t Direccion Sede: "<<address<<endl;
+				found++;
+			}
+			file >> numSede >> name >> phone >> address;
+		}
+		if(found==0)
+		{
+			cout<<"\n\t\t\t Sede no encontrada...";
+		}
+		file.close();
+	}
+}
+
+void colegio::deletSede(){
+    system("cls");
+	fstream file,file1;
+	string busquedaSede;
+	int found=0;
+	cout<<"\n-------------------------Detalles de Sede a Borrar-------------------------"<<endl;
+	file.open("sede.txt",ios::in);
+	if(!file)
+	{
+		cout<<"\n\t\t\tNo hay informacion...";
+		file.close();
+	}
+	else
+	{
+		cout<<"\n Ingrese el NUMERO de la Sede que quiere borrar: ";
+		cin>>busquedaSede;
+		file1.open("modSede.txt",ios::app | ios::out);
+		file >> numSede >> name >> phone >> address;
+		while(!file.eof())
+		{
+			if(busquedaSede!= numSede)
+			{
+				file1<<std::left<<std::setw(15)<< numSede <<std::left<<std::setw(15)<< name <<std::left<<std::setw(15)<< phone <<std::left<<std::setw(15)<< address << "\n";
+			}
+			else
+			{
+				found++;
+				cout << "\n\t\t\tBorrado de informacion exitoso";
+			}
+			file >> numSede >> name >> phone >> address;
+		}
+		if(found==0)
+		{
+			cout<<"\n\t\t\t Numero de Sede no encontrado...";
+		}
+		file1.close();
+		file.close();
+		remove("sede.txt");
+		rename("modSede.txt","sede.txt");
+	}
 }
 
 void colegio::facultades(){
-    cout <<"hola";
+    int choice;
+	char x;
+	do
+    {
+	system("cls");
+	cout<<"\t\t\t---------------------------------"<<endl;
+	cout<<"\t\t\t| BIENVENIDOS AL MENU FACULTADES |"<<endl;
+	cout<<"\t\t\t---------------------------------"<<endl;
+	cout<<"\t\t\t 1. Registrar"<<endl;
+	cout<<"\t\t\t 2. Mostrar"<<endl;
+	cout<<"\t\t\t 3. Modificar"<<endl;
+	cout<<"\t\t\t 4. Buscar"<<endl;
+	cout<<"\t\t\t 5. Eliminar"<<endl;
+	cout<<"\t\t\t 6. Regresar al MENU CATALOGOS"<<endl;
+	cout<<"\n\t\t\tIngresa tu Opcion: "; cin>>choice;
+    switch(choice)
+    {
+    case 1:
+    	insertFacultad();
+		break;
+	case 2:
+		displayFacultad();
+		break;
+	case 3:
+		modifyFacultad();
+		break;
+    case 4:
+        searchFacultad();
+        break;
+    case 5:
+        deletFacultad();
+        break;
+    case 6:
+        return catalogos();
+	default:
+		cout<<"\n\t\t\t Opcion invalida...Por favor prueba otra vez..";
+	}
+	getch();
+    }
+    while(choice!= 6);
 }
+
+void
 
 void colegio::cursos(){
     cout <<"hola";
